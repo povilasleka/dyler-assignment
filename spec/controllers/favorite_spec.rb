4a2bc0e4b3776c112874fb1e2d95e17bb84db28d
povilasleka <povilasleka@gmail.com>
2021-06-13 23:56:27 +0300

    new api  unit tests

--- spec/controllers/favorite_spec.rb
+++ spec/controllers/favorite_spec.rb
@@ -1,25 +1,47 @@
 require 'rails_helper'
 
-describe 'Favorite API', type: :request do
+describe V1::FavoriteController, type: :request do
     describe 'GET /favorite' do
-        it 'returns favorite manufacturers' do
+        it 'returns success status' do
             get '/v1/favorite/guest_id'
             expect(response).to have_http_status(:success)
+        end
+
+        it 'returns data with manufacturers list' do
+            FactoryBot.create(:favorite)
+
+            get '/v1/favorite/guest_id'
+
+            parsed_response = JSON.parse(response.body)
+
+            expect(parsed_response['data'].length).to eq(1)
         end
     end
 
     describe 'POST /favorite' do
-        it 'creates a new favorite manufacturer' do
-            expect {
-                post '/v1/favorite', params: {
-                    guest_id: 'guest_id_1',
-                    manufacturer_id: '995',
-                    name: 'Favorite1',
-                    country: 'UK'
-                }
-            }.to change { Favorite.count }.from(0).to(1)
+        params = {
+            guest_id: 'guest_id_1',
+            manufacturer_id: '995',
+            name: 'Favorite1',
+            country: 'UK'
+        }
+
+        it 'returns a created response' do 
+            post '/v1/favorite', params: params
 
             expect(response).to have_http_status(:created)
+        end
+
+        it 'creates a new favorite manufacturer' do
+            expect {
+                post '/v1/favorite', params: params
+            }.to change { Favorite.count }.from(0).to(1)
+        end
+    end
+
+    describe 'DELETE /favorite' do
+        it 'returns no content response' do
+            
         end
     end
 end
--- spec/factories/favorite.rb
+++ spec/factories/favorite.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
     factory :favorite do
-        
+        manufacturer_id { "1" }
+        guest_id { "guest_id" }
+        name { "somename" }
+        country { "uk" }
     end
 end
